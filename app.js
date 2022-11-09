a1fd9756cf97f4e20867d8496bcaf0a8037ecf0c
unknown <kiva.loor@nyu.edu>
2022-11-09 12:49:51 -0500

    added Firebase

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const express = require("express")
+// imports/requires
+const express = require("express");
+const firebase = require("firebase/app");
+
 const app = express()
 const port = process.env.PORT || 4000
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyAz97WhUMlieMkG7Gb1I1ppNgNXZ8jywXg",
+  authDomain: "exercise-4-f22.firebaseapp.com",
+  projectId: "exercise-4-f22",
+  storageBucket: "exercise-4-f22.appspot.com",
+  messagingSenderId: "977346725076",
+  appId: "1:977346725076:web:111e83b859fbbcd564a741"
+};
+
+// Initialize Firebase App
+firebase.initializeApp(firebaseConfig);
+
 
 const indexRoute = require("./routes/index")
 
